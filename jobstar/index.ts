b04faa4ae896aa74acfe106d0018dde3dba0e307
andrewta999 <andrewta365@gmail.com>
2020-11-06 15:30:51 -0500

    commented some code

--- jobstar/index.ts
+++ jobstar/index.ts
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
 import {app} from './app'
 
 const start = async () => {
-    try {
-        await mongoose.connect('mongodb://mongodb-srv:27017/auth')
-        console.log('MongoDb is connected')
-    } catch (err) {
-        console.log(err)
-    }
+    // try {
+    //     await mongoose.connect('mongodb://mongodb-srv:27017/auth')
+    //     console.log('MongoDb is connected')
+    // } catch (err) {
+    //     console.log(err)
+    // }
+    
     app.listen(3000, () => {
         console.log("App listening on port 3000!")
     })
