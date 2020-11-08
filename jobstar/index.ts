0161547e9316036c856fdba78a3e11f1238f4040
andrewta999 <andrewta365@gmail.com>
2020-11-08 16:54:58 -0500

    some changes

--- jobstar/index.ts
+++ jobstar/index.ts
@@ -3,12 +3,6 @@
 import {app} from './app'
 
 const start = async () => {
-    // try {
-    //     await mongoose.connect('mongodb://mongodb-srv:27017/auth')
-    //     console.log('MongoDb is connected')
-    // } catch (err) {
-    //     console.log(err)
-    // }
     
     app.listen(3000, () => {
         console.log("App listening on port 3000!")
